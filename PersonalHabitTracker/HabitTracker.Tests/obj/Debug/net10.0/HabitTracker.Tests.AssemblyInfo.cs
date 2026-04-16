6595e20a02fbf86ff3a1261ebfdef6ffd31a6dee
andrew-7788 <157413974+andrew-7788@users.noreply.github.com>
2026-04-16 20:55:04 +0000

    Prepare v3.0.0 release

--- PersonalHabitTracker/HabitTracker.Tests/obj/Debug/net10.0/HabitTracker.Tests.AssemblyInfo.cs
+++ PersonalHabitTracker/HabitTracker.Tests/obj/Debug/net10.0/HabitTracker.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTracker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8c58b2d0309299cbd7504e8b3dcda2a6c2d4b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeefa0bdf5b95559c73d0b361be1d623cd84403b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTracker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTracker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
